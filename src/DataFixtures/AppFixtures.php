e607595bcd0d2c776762d0554b8713c0a2a3a0b5
csiformk <formk@kermed.fr>
2023-12-01 11:03:55 +0100

    Ajout de fixture dans une boucle

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Formation;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,15 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 0; $i < 50; $i++) :
+            $formation = new Formation();
+
+            $formation->setNom('Anglais-'. $i);
+            $formation->setDescription('Formation anglais blah blah ' . $i);
+            $formation->setLieux('Havre - ' . $i);
+
+            $manager->persist($formation);
+        endfor;
 
         $manager->flush();
     }
