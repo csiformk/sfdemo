bea840ce3a2625598fd4f343b0c4f2cc2a8f0252
csiformk <formk@kermed.fr>
2023-12-01 11:20:31 +0100

    Ajout de donnée avec Faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,17 +5,20 @@ namespace App\DataFixtures;
 use App\Entity\Formation;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        for ($i = 0; $i < 50; $i++) :
+        $faker = Factory::create();
+        
+        for ($i = 0; $i < 5; $i++) :
             $formation = new Formation();
 
-            $formation->setNom('Anglais-'. $i);
-            $formation->setDescription('Formation anglais blah blah ' . $i);
-            $formation->setLieux('Havre - ' . $i);
+            $formation->setNom($faker->words(3, true));
+            $formation->setDescription($faker->paragraphs(20, true));
+            $formation->setLieux($faker->city());
 
             $manager->persist($formation);
         endfor;
